d874bc7f6498ba04987330927c1e22a9cbbdfc82
Bhoumik <bhoumiks5910@gmail.com>
2015-10-07 21:36:42 +0530

    Correct error for 3 days

--- TOP/views.py
+++ TOP/views.py
@@ -7,7 +7,9 @@ from main import plan
 def home(request):
     response_data={}
     days=request.GET['days']
-    response_data['Result'] = plan(int(days)).tolist()
+    data = plan(int(days))
+    response_data['Result'] = [x.tolist() for x in data]
+    print response_data['Result']
     return HttpResponse(json.dumps(response_data),content_type="application/json")
 
 # Create your views here.
