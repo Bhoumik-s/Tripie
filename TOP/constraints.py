9ae3bee3370d69afcf8291e0501f0a57528cb284
Bhoumik <bhoumiks5910@gmail.com>
2015-11-26 00:35:47 +0530

    Rename Constraints

--- TOP/constraints.py
+++ TOP/constraints.py
@@ -12,7 +12,7 @@ def status(sol,data):
                 f=f+data.HAPPINESS[j]*y[j,i]
         return f
 
-    def constraint7():  # 
+    def TimeConstraint():  # 
         boolean=True
         for k in range(data.DAYS):
             sum1=0
@@ -27,18 +27,18 @@ def status(sol,data):
         else:
             return [False,7]
 
-    def constraint6():
+    def ContinuityConstraint():
         boolean=True
         for i in range(data.n+2):
             for j in range(data.n+2):
                 for k in range (data.DAYS):
                     boolean=(boolean and (pi[i,k]+data.SERVICETIME[i]+data.TRAVELTIME[i,j]-pi[j,k]<=data.M*(1-x[i,j,k])))
         if boolean:
-            return constraint7()
+            return TimeConstraint()
         else:
             return [False,6]
             
-    def constraint5():
+    def OneVertexConstraint():
         boolean=True
         for i in range(1,data.n+1):
             for k in range(data.DAYS):
@@ -46,11 +46,11 @@ def status(sol,data):
                 sum2=np.sum(x[i,1:data.n+2,k])
                 boolean=(boolean and ((sum1==sum2) and (sum1==y[i,k])))
         if boolean:
-            return constraint6()
+            return ContinuityConstraint()
         else:
             return [False,5]
 
-    def constraint4():
+    def BudgetConstraint():
         boolean=True
         sum1=0
         for k in range(data.DAYS):
@@ -58,38 +58,38 @@ def status(sol,data):
                 sum1=sum1+data.COST[i]*y[i,k]
         boolean=(boolean and (sum1<=data.BUDGET))
         if boolean:
-            return constraint5()
+            return OneVertexConstraint()
         else:
             return [False,4]
 
-    def constraint3():
+    def OnceConstraint():
         boolean=True
         for h in range(1,data.n+1):
             boolean=(boolean and (np.sum(y[h,:])<=1))
         if boolean:
-            return constraint4()
+            return BudgetConstraint()
         else:
             return [False,3]
 
         
-    def constraint2():
+    def DaysConstraint():
         sum1=np.sum(x[0,1:data.n+2,:])
         sum2=np.sum(x[0:data.n+1,data.n+1,:])
         if (((sum1==sum2) and (sum1==data.DAYS))):
-            return constraint3()
+            return OnceConstraint()
         else:
             return [False,2]
 
-    def constraint1():
+    def CloseTimetraint():
         boolean=True
         for k in range(data.DAYS):
             for i in range (data.n+2):
                 boolean=(boolean and (pi[i,k]<=data.CLOSETIME[i]))
         if boolean:
-            return constraint2()
+            return DaysConstraint()
         else:
             return [False,1]
 
     
-    return (constraint1()) 
+    return (CloseTimetraint()) 
     
