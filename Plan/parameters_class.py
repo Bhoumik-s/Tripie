92fcebb01567ffb4e7e644ae7426ad152e31ef3c
Bhoumik <bhoumiks5910@gmail.com>
2015-12-13 16:13:46 +0530

    Make gunicorn bash file

---
+++ Plan/parameters_class.py
@@ -0,0 +1,46 @@
+import datetime
+import numpy as np
+
+class ParameterClass():
+	def __init__(self,request):	
+	
+		self.city = request.GET['City']
+		self.deviceId = request.GET['deviceId']
+		self.budget = 1500
+
+		self.startTime = int(request.GET['StartTime'])
+		self.endTime = int(request.GET['EndTime'])
+		self.startDate = datetime.datetime.strptime(request.GET['StartDate'], '%d%m%Y')
+		self.endDate = datetime.datetime.strptime(request.GET['EndDate'], '%d%m%Y')
+		self.days = abs((self.endDate - self.startDate).days)
+
+		self.visited = np.array((request.GET['Visited']).split(","),dtype=int)
+		self.interest = np.array((request.GET['Interest']).split(","),dtype=int)
+		
+
+		self.startLocation = np.array((request.GET['StartLocation']).split(","),dtype=float)
+		self.stayLocation = np.array((request.GET['StayLocation']).split(","),dtype=float)
+		
+		firstCondition = [
+						[self.startLocation[0],self.startLocation[1],
+						self.stayLocation[0],self.stayLocation[1],
+						self.startTime, 1050 + self.interest[3] * 90]
+						]
+
+		midCondition = [
+						self.startLocation[0],self.startLocation[1],
+						self.stayLocation[0],self.stayLocation[1],
+						240 + self.interest[3] * 60, 1050 + self.interest[3] * 90
+						]
+
+		for i in range (self.days-1):
+			firstCondition.append(midCondition)
+
+		self.boundryConditions = np.asarray(firstCondition)	
+		self.boundryConditions[-1,5] = self.endTime
+
+		self.timeMultiplier = min(6, 0.5 + sum([5,2.5,2,1.5,2] * self.interest[:]) )
+		
+
+		#print self.days
+		#print self.boundryConditions
---
+++ gunicorn_start.bash
@@ -0,0 +1,32 @@
+NAME="Tripie"                                     # Name of the application
+DJANGODIR=~/TripieServer            # Django project directory
+SOCKFILE=~/TripieServer/gunicorn.sock  # we will communicte using this unix socket
+#USER=bhoumik                                        # the user to run as
+#GROUP=webapps                                     # the group to run as
+NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=Tripie.settings             # which settings file should Django use
+DJANGO_WSGI_MODULE=Tripie.wsgi                     # WSGI module name
+
+#echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source .env/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER \
+  --bind 0.0.0.0:8000\
+  --log-level=debug \
+  --log-file=-\
+  &
+
