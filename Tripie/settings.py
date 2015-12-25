dc67dd6d24afc89dd29f525bbdf52bc19332ad01
Bhoumik <bhoumiks5910@gmail.com>
2015-12-25 13:56:07 +0530

    Comment out apps from settings

--- Tripie/settings.py
+++ Tripie/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'Plan.apps.PlanConfig',
-    'signup.apps.SignupConfig',
+    #'Plan.apps.PlanConfig',
+    #'signup.apps.SignupConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
